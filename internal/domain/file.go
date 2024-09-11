2a464bf928e4525c714162edeed69cb1317fba8e
Felipe Fernandes Versiane <felipeversiane09@gmail.com>
2024-09-10 21:49:00 -0300

    feat: adding a domain file entity

--- internal/domain/file.go
+++ internal/domain/file.go
@@ -1,1 +1,17 @@
 package domain
+
+import (
+	"github.com/google/uuid"
+)
+
+type File struct {
+	ID  uuid.UUID
+	URL string
+}
+
+func NewFile(url string) *File {
+	return &File{
+		ID:  uuid.New(),
+		URL: url,
+	}
+}
